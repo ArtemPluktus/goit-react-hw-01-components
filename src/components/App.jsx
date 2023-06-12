9718f43444726d5389229deb3128d800ce4a548f
ArtemPluktus <artempluktus@gmail.com>
2023-06-12 12:01:26 +0200

    install

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,6 @@
+import { Profile } from './Profile/Profile';
+import profileData from './Profile/profileData.json';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <Profile />;
 };
